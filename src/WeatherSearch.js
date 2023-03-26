caa07ed4d35ac596d00609ff84ebc9e23d075472
irguk <igukova@gmail.com>
2023-03-26 22:41:10 +0300

    added WeatherSearch.js

---
+++ src/WeatherSearch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function WeatherSearch() {
+
+  
+    return (
+        <form>
+            <input type="search"/>
+            <input type="submit" value="Search"/>
+        </form>
+    )
+      
+  }
